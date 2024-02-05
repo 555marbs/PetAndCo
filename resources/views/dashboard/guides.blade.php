6563853ddd5e62b924caaadd98490c984a3aa416
555marbs <janrumbaua6@gmail.com>
2024-02-05 21:37:14 +0800

    change some codes in guides

--- resources/views/dashboard/guides.blade.php
+++ resources/views/dashboard/guides.blade.php
@@ -8,21 +8,21 @@
 
 @section('content')
 <div class="container">
-    <div class="animal-item">
+    <div class="grid-item">
         <img src="/img/dog1.jpg" alt="Dog">
-        <div class="description">BRIEF DESCRIPTION:</div>
+        <p>BRIEF DESCRIPTION:<p>
     </div>
-    <div class="animal-item">
-        <img src="/img/dog1.jpg" alt="Cat">
-        <div class="description">BRIEF DESCRIPTION:</div>
+
+    <div class="grid-item">
+        <img src="/img/dog1.jpg" alt="Dog">
+        <p>BRIEF DESCRIPTION:<p>
     </div>
-    <div class="animal-item">
-        <img src="/img/dog1.jpg" alt="Rabbit">
-        <div class="description">BRIEF DESCRIPTION:</div>
+
+    <div class="grid-item">
+        <img src="/img/dog1.jpg" alt="Dog">
+        <p>BRIEF DESCRIPTION:<p>
     </div>
-    <div class="animal-item">
-        <img src="/img/dog1.jpg" alt="Parrot">
-        <div class="description">BRIEF DESCRIPTION:</div>
-    </div>
+
+
 </div>
 @endsection
