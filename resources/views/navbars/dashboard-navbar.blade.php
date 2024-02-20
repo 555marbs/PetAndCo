d211dd77bfc9c72f66fec3b95a71fee8ed2463e2
555marbs <janrumbaua6@gmail.com>
2024-02-20 23:01:47 +0800

    added name in dashboard.navbar

--- resources/views/navbars/dashboard-navbar.blade.php
+++ resources/views/navbars/dashboard-navbar.blade.php
@@ -24,7 +24,7 @@
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    More
+                    {{ Auth::user()->name}}
                 </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="{{ route('profile.edit') }}">Profile</a>
