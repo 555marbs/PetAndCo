b08313c60e9b1488e01f7910d198c3b5e3ed98ff
meljhon <meab.royeca.up@phinmaed.com>
2024-02-27 22:24:27 +0800

    updated categories

--- resources/views/dashboard/categories.blade.php
+++ resources/views/dashboard/categories.blade.php
@@ -9,45 +9,26 @@
 @section('content')
     <div class="container">
         <div class="button">
+        <a href="{{ route('dog') }}" class="card-link">
+
             <img src="/img/1.png" alt="DOG">
-            <div class="dropdown">
-                <!-- Dropdown content for button 1 -->
-                <h4>Breeds of Dogs</h4>
-                <a href="{{ route('dashboard') }}">Chihuahua</a>
-                <p>Corgi</p>
-                <p>Dalmatian</p>
-                <p>Golden Retriever</p>
-                <p>Labrador</p>
-                <p>Pit bull</p>
-                <p>Poodle</p>
-                <p>Pug</p>
-            </div>
         </div>
 
         <div class="button">
+        <a href="{{ route('cat') }}" class="card-link">
+
             <img src="/img/2.png" alt="Button Image">
-            <div class="dropdown">
-                <!-- Dropdown content for button 2 -->
-                <p>Dropdown Content 2</p>
-                <p>Another item</p>
-            </div>
         </div>
 
         <div class="button">
+        <a href="{{ route('bird') }}" class="card-link">
+
             <img src="/img/3.png" alt="Button Image">
-            <div class="dropdown">
-                <!-- Dropdown content for button 2 -->
-                <p></p>
-                <p>Another item</p>
-            </div>
         </div>
 
         <div class="button">
+        <a href="{{ route('fish') }}" class="card-link">
+
             <img src="/img/4.png" alt="Button Image">
-            <div class="dropdown">
-                <!-- Dropdown content for button 2 -->
-                <p>Dropdown Content 2</p>
-                <p>Another item</p>
-            </div>
         </div>
     @endsection
