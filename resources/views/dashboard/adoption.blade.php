12e9e3112aabb578f80f3b2acf35e8c827b52ae7
555marbs <janrumbaua6@gmail.com>
2024-03-02 09:40:54 +0800

    fixed the css of adoption

--- resources/views/dashboard/adoption.blade.php
+++ resources/views/dashboard/adoption.blade.php
@@ -2,20 +2,29 @@
 
 @extends('navbars.dashboard-navbar')
 
-@section('content')
-<div class="container">
-    <div>
-        @foreach($adoptions as $adoption)
-            <div>
-                <img src="{{ asset('/' . $adoption->image) }}" alt="{{ $adoption->title }}">
-                <h2>{{ $adoption->title }}</h2>
-                <h3>{{ $adoption->contact}}</h3>
-                <p>{{ $adoption->content }}</p>
 
-                <!-- Adopt button -->
-                <a href="{{ url('/adopt/' . $adoption->id) }}" class="btn btn-primary">Adopt</a>
-            </div>
-        @endforeach
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('css/adoption.css')}}">
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            @foreach ($adoptions as $adoption)
+                <div class="col-md-4">
+                    <div class="card">
+                        <img class="card-img-top" src="{{ asset('/' . $adoption->image) }}" alt="{{ $adoption->title }}">
+                        <div class="card-body">
+                            <h2 class="card-title">{{ $adoption->title }}</h2>
+                            <h3 class="card-text">{{ $adoption->contact }}</h3>
+                            <p class="card-text">{{ $adoption->content }}</p>
+                            <div class="dropdown-divider"></div>
+                            <!-- Adopt button -->
+                            <a href="{{ url('/adopt/' . $adoption->id) }}" class="btn btn-primary">Adopt</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
-</div>
 @endsection
