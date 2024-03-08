b3bb41d44f104c82f37cd4bd0fa0c0431f341dc0
meljhon <meab.royeca.up@phinmaed.com>
2024-03-08 19:59:10 +0800

    Modified Button Adpotion

--- resources/views/dashboard/adoption.blade.php
+++ resources/views/dashboard/adoption.blade.php
@@ -18,16 +18,16 @@
                             <h2 class="card-title">{{ $adoption->title }}</h2>
                             <h5 class="card-text">{{ $adoption->contact }}</h5>
                             <p class="card-text">{{ $adoption->content }}</p>
-                            <!-- Adopt button -->
-                            <a href="{{ route('adoption.application.form', ['adoption' => $adoption->id]) }}" class="adoption-apply-button">Apply for Adoption</a>
-                        </div>
+                            <div class="dropdown-divider"></div>
+                            <div class="button-container">
+                                   <button class="btn btn-primary center-btn" onclick="window.location.href='{{ route('adoption.application.form', ['adoption' => $adoption->id]) }}'">Apply for Adoption</button>
+                            </div>                        
+                            </div>
                     </div>
                 </div>
             @endforeach
         </div>
     </div>
-
-<!-- Button at the bottom of the navigation bar -->
 <div class="navbar-bottom-button d-flex justify-content-center">
     <a href="{{ route('adoption_create') }}" class="btn btn-primary" style="width: 900px;">Post a Pet For Adoption</a>
 </div>
