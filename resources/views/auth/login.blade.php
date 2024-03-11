62bce89f08c472a1fa98f1a2490259602f340a64
Jemjem1232 <jama.soldao.up@phinamed.com>
2024-03-12 00:22:32 +0800

    Adoption and login css need to be fixed parin not fully sure na yun na yung ayos niya....

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,41 @@
 @extends('layouts.app')
 
 @section('styles')
-<link rel="stylesheet" href="{{ asset('css/login.css') }}">
-<link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 @endsection
 
-<div class="az-signin-wrapper">
-    <div class="az-card-signin">
-      <a class="az-logo">
-        <img src="/img/logo.png" alt="Pet&Co.">
-      </a>
-      <div class="az-signin-header">
-        <h2>Welcome back!</h2>
-        <h4>Please sign in to continue</h4>
-
-        <form action="{{ route('login') }}" method="POST">
-          @csrf <!-- Generates CSRF token field -->
-
-          <div class="form-group">
-            <label>Email</label>
-            <input type="text" class="form-control" name="email" id="email" maxlength="30">
-          </div><!-- form-group -->
-          <div class="form-group">
-            <label>Password</label>
+@section('content')
+    <div class="login-container">
+        <div class="logo-container">
+            <img src="{{ asset('img/logo.png') }}" alt="PetCo Logo">
+        </div>
+        <h2>Welcome back Furparent!</h2>
+        <p>Please sign in to continue</p>
+        <form action="{{ route('login') }}" method="POST" class="login-form">
+            @csrf
             <div class="input-group">
-              <input type="password" class="form-control" name="password" id="password" maxlength="30">
-              <div class="input-group-append">
-                <span class="input-group-text" id="togglePassword">
-                  <i class="fa fa-eye" aria-hidden="true" onclick="togglePasswordVisibility()"></i>
-                </span>
-              </div>
-            </div><!-- input-group -->
-          </div><!-- form-group -->
-          <button type="submit" class="btn btn-az-primary btn-block">Sign In</button>
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" required>
+            </div>
+            <div class="input-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <button type="submit">Sign In</button>
         </form>
-      </div><!-- az-signin-header -->
-      <div class="az-signin-footer">
-        <p>Don't have an account? <a href="{{ route('register') }}">Create an Account</a></p>
-      </div><!-- az-signin-footer -->
-    </div><!-- az-card-signin -->
-  </div><!-- az-signin-wrapper -->
-
-  <script src="{{ asset('js/login.js')}}"></script>
+        <div class="login-footer">
+            <p>Don't have an account? <a href="path-to-sign-up">Create an Account</a></p>
+        </div>
+    </div>
+@endsection
 
 <script>
-function togglePasswordVisibility() {
-  var passwordInput = document.getElementById("password");
-  if (passwordInput.type === "password") {
-    passwordInput.type = "text";
-  } else {
-    passwordInput.type = "password";
-  }
-}
+    function togglePasswordVisibility() {
+        var passwordInput = document.getElementById("password");
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+        } else {
+            passwordInput.type = "password";
+        }
+    }
 </script>
